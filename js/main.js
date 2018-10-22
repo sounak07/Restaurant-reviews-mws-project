34a3e32f97dd3c85cd4dd2891cc42e30e6248812
sounak07 <sounakume@gmail.com>
2018-10-22 17:51:29 +0530

    Code ready fro stage2

--- js/main.js
+++ js/main.js
@@ -175,7 +175,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = `${DBHelper.imageUrlForRestaurant(restaurant)}.jpg`;
   //added alt tags to images
   image.alt = restaurant.name + ' image';
   li.append(image);
